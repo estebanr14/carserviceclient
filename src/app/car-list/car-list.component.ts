a2f38882cca4e6ad7251f6f617e44b7d5c3d3610
estebanr14 <you@example.com>
2019-07-30 20:10:28 -0500

    Actualizado lista de owners en carros

--- src/app/car-list/car-list.component.ts
+++ src/app/car-list/car-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CarService } from '../shared/car/car.service';
 import { GiphyService } from '../shared/giphy/giphy.service';
+import { OwnerService } from '../shared/owner/owner.service'
 
 @Component({
   selector: 'app-car-list',
@@ -10,13 +11,23 @@ import { GiphyService } from '../shared/giphy/giphy.service';
 export class CarListComponent implements OnInit {
   cars: Array<any>;
 
-  constructor(private carService: CarService, private giphyService: GiphyService) { }
+  constructor(private carService: CarService, private ownerService: OwnerService, private giphyService: GiphyService) { }
 
   ngOnInit() {
     this.carService.getAll().subscribe(data => {
       this.cars = data;
       for (const car of this.cars) {
         this.giphyService.get(car.name).subscribe(url => car.giphyUrl = url);
+        if (car.ownerDni) {
+          this.ownerService.getWithDNI(car.ownerDni).subscribe((owner: any) => {
+            if (owner[0]) {
+              car.ownerName = owner[0].name
+            }
+
+          })
+
+        }
+
       }
     });
   }
